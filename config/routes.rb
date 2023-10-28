12256e96c489c62f3fe26cd371cdddaa4c34e2bf
Marco Díaz <md.marcodiaz2000@gmail.com>
2023-10-28 04:50:30 -0600

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
+  namespace :api do
+    namespace :v1 do
+      # Get reservations for a user
+      get 'users/:user_id/reservations', to: 'reservations#user_reservations'
+      get 'users/:user_id/items', to: 'reservations#user_items'
+      post 'reset_database', to: 'application#reset_database'
+      
+      resources :items
+      resources :users
+      resources :reservations
+    end
+  end
+end
